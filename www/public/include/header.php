8c871a600ac346136b33e0a6a9a5b91877fb87a8
MrTryAgain <mrtryagain2021@gmail.com>
2021-06-21 13:07:45 +0200

    Neues Repo mit Ordner Struktur Branch Urs

---
+++ www/public/include/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="build/css/style.css">
+  <title>Nature Quest Quizsite</title>
+</head>
+
+<body>
+
+<nav>
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="quizUrs.php">Quiz Urs</a></li>
+  <li><a href="quizMarc.php">Quiz Marc</a></li>
+  <li><a href="quizJacqueline.php">Quiz Jacqueline</a></li>
+</ul>
+</nav>
---
+++ www/public/result.php
@@ -0,0 +1,11 @@
+<?php require_once "include/header.php"?>
+
+
+  <?php
+
+  echo "Hier kommen die Resultate / Gratulation<br>";
+  echo "evtl. auch ein Feuerwerk ;)<br>";
+
+  ?>
+
+<?php require_once "include/footer.php"?>
